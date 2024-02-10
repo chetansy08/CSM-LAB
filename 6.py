a30f412cd652767434641a6872415a53b9cc4390
chetansy08 <137137686+chetansy08@users.noreply.github.com>
2024-02-11 02:08:15 +0530

    6.py

---
+++ 6.py
@@ -0,0 +1,31 @@
+import seaborn as sns
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
+
+df = load_breast_cancer()
+X = df.data
+y = df.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression(max_iter=5000)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate performance
+conf_matrix = confusion_matrix(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+
+print("Accuracy: {:.2f}".format(accuracy))
+
+# Visualize confusion matrix using heatmap
+sns.heatmap(conf_matrix, annot=True, cmap="Blues")
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
